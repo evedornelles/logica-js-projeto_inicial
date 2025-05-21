c7b5a441e7680725bea298e8e5de7bfcc11df7b7
Évelim Dornelles <dorrrnelles@gmail.com>
2025-05-20 21:17:14 -0300

    upload

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-alert('Boas vindas ao jogo do número secreto');
+/*alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto ) { 
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+}
+
+alert ('Boas vindas ao nosso site!');
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas =50;
+let saldoDisponivel = 1000;
+
+alert ('Erro! Preencha todos os campos');
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert (mensagemDeErro);
+
+let nome = prompt('Digite o seu nome');
+let idade = prompt('Digite a sua idade');
+
+let idade = prompt('Digite a sua idade');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+} */
